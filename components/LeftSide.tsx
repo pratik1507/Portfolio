a9e54f5c28dc390ca042794fd90fd20b163ebe80
Powar Pratik Ravsaheb <2100032537@kluniversity.in>
2024-09-21 23:45:25 +0530

    Update LeftSide.tsx

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -43,7 +43,7 @@ const LeftSide = () => {
         </a>
 
         <a
-          href="https://www.geeksforgeeks.org/user/user_5398za8vobt/"
+          href="https://www.codechef.com/users/klu_2100032537/"
           target="_blank"
         >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
